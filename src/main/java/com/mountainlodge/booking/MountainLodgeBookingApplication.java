fc778319b351f5833ee77a6fce9afd49df3e79be
bojan55 <bojan.bajic55@gmail.com>
2026-08-05 18:22:47 +0200

    Initial project setup with folder structure

---
+++ src/main/java/com/mountainlodge/booking/MountainLodgeBookingApplication.java
@@ -0,0 +1,13 @@
+package com.mountainlodge.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountainLodgeBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MountainLodgeBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mountainlodge/booking/MountainLodgeBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountainlodge.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MountainLodgeBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
